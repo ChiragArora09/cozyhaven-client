5a062df3b7d0c758abe588ee56c4f35dca193ea8
Chirag Arora <chiragarora44.ca@gmail.com>
2024-10-02 02:39:43 +0530

    Added filter flight by time

--- cozyhaven-client/src/app/views/Flight-Booking/fetched-flights/fetched-flights.component.ts
+++ cozyhaven-client/src/app/views/Flight-Booking/fetched-flights/fetched-flights.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FlightDataService } from '../../../service/flightData.service';
 import { NgFor } from '@angular/common';
 import { FlightCardComponent } from '../../../components/flight-card/flight-card.component';
+import { NavbarComponent } from '../../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-fetched-flights',
   standalone: true,
-  imports: [NgFor, FlightCardComponent],
+  imports: [NgFor, FlightCardComponent, NavbarComponent],
   templateUrl: './fetched-flights.component.html',
   styleUrl: './fetched-flights.component.css'
 })
@@ -17,6 +18,49 @@ export class FetchedFlightsComponent {
 
   ngOnInit() {
     const flightsData = this.flightDataService.getFlights();
-    this.flights = flightsData;
+
+    if (flightsData && flightsData.length > 0) {
+      this.flights = flightsData;
+      localStorage.setItem('flightsData', JSON.stringify(flightsData));
+    }
+    else{
+      const storedFlights = localStorage.getItem('flightsData');
+      if (storedFlights){
+        this.flights = JSON.parse(storedFlights);
+      } 
+    }
+  }
+
+  filterbyMorning(){
+    this.flights = JSON.parse(localStorage.getItem('flightsData'))
+    const startTime = new Date("1970-01-01T00:30:00Z")
+    const endTime = new Date("1970-01-01T06:30:00Z");
+
+    this.flights =  this.flights.filter(f => {
+      const flightTime = new Date(`1970-01-01T${f.sourceArrival}Z`)
+      return flightTime >= startTime && flightTime <= endTime;
+    })
+  }
+
+  filterbyAfternoon(){
+    this.flights = JSON.parse(localStorage.getItem('flightsData'))
+    const startTime = new Date("1970-01-01T06:30:00Z")
+    const endTime = new Date("1970-01-01T12:30:00Z");
+
+    this.flights = this.flights.filter(f => {
+      const flightTime = new Date(`1970-01-01T${f.sourceArrival}Z`)
+      return flightTime >= startTime && flightTime <= endTime;
+    })
+  }
+
+  filterbyLate(){
+    this.flights = JSON.parse(localStorage.getItem('flightsData'))
+    const startTime = new Date("1970-01-01T12:30:00Z")
+    const endTime = new Date("1970-01-01T18:29:00Z");
+
+    this.flights = this.flights.filter(f => {
+      const flightTime = new Date(`1970-01-01T${f.sourceArrival}Z`)
+      return flightTime >= startTime && flightTime <= endTime;
+    })
   }
 }
